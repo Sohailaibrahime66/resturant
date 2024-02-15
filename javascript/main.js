72373b4d8fb272a49b85696d53b1719afbbdf8d6
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2024-02-15 07:24:23 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+function navigateToPage() {
+    // Redirect to the desired page
+    window.location.href = 'booktable.html';
+}
+
+
+function login() {
+  // Redirect to the desired page
+  window.location.href = 'login.html';
+}
+
+
+
+function signin() {
+  // Redirect to the desired page
+  window.location.href = 'rejester.html';
+}
+
+function goin() {
+  // Redirect to the desired page
+  window.location.href = 'home.html';
+}
+
+
+
